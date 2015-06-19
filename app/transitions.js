9221e826e46377ca4da4656de0c6d5cf63a6f7e4
e-karma <djbradyla@gmail.com>
2015-06-19 06:50:01 -0400

    all platform and why transitions improved

--- app/transitions.js
+++ app/transitions.js
@@ -217,6 +217,22 @@ export default function() {
       use: ['toRight', { duration } ]
     })
   );
+  this.transition(
+    this.fromRoute('why'),
+    this.toRoute('why.unified-platform'),
+    this.use('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toLeft', { duration } ]
+    }),
+    this.reverse('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toRight', { duration } ]
+    })
+  );
   // this.transition(
   //   this.fromRoute('index'),
   //   this.toRoute('next-steps'),
@@ -261,75 +277,293 @@ export default function() {
 
   /* Route 1 */
 
-  this.transition(
-    this.fromRoute('why'),
-    this.toRoute('why.unified-platform'),
-    this.use('toLeft'),
-    this.reverse('toRight')
-  ),
-  this.transition(
-    this.fromRoute('why'),
-    this.toRoute('why.cutting-edge-features'),
-    this.use('fade'),
-    this.reverse('fade')
-  ),
-  this.transition(
-    this.fromRoute('why'),
-    this.toRoute('why.efficiency'),
-    this.use('toLeft'),
-    this.reverse('toRight')
-  ),
+
   /* Route 2 */
   this.transition(
     this.fromRoute('platform'),
     this.toRoute('roadmap'),
     this.use('toLeft'),
     this.reverse('toRight')
-  ),
+  );
   /* Route 3 */
   this.transition(
     this.fromRoute('roadmap'),
     this.toRoute('store'),
     this.use('toLeft'),
     this.reverse('toRight')
-  ),
+  );
   /* Route 4 */
   this.transition(
     this.fromRoute('platform'),
     this.toRoute('application'),
     this.use('toLeft'),
     this.reverse('toRight')
-  ),
+  );
   this.transition(
-    this.fromRoute('platform'),
+    this.fromRoute('why.index'),
+    this.toRoute('why.unified-platform'),
+    this.use('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toLeft', { duration } ]
+    }),
+    this.reverse('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toRight', { duration } ]
+    })
+  );
+  this.transition(
+    this.fromRoute('why.index'),
+    this.toRoute('why.efficiency'),
+    this.use('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toLeft', { duration } ]
+    }),
+    this.reverse('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toRight', { duration } ]
+    })
+  );
+  this.transition(
+    this.fromRoute('why.index'),
+    this.toRoute('why.cutting-edge-features'),
+    this.use('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toLeft', { duration } ]
+    }),
+    this.reverse('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toRight', { duration } ]
+    })
+  );
+  this.transition(
+    this.fromRoute('platform.index'),
     this.toRoute('platform.dashboard'),
-    this.use('toUp'),
-    this.reverse('toDown')
-  ),
+    this.use('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toLeft', { duration } ]
+    }),
+    this.reverse('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toRight', { duration } ]
+    })
+  );
+  this.transition(
+    this.fromRoute('platform.index'),
+    this.toRoute('platform.estore'),
+    this.use('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toLeft', { duration } ]
+    }),
+    this.reverse('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toRight', { duration } ]
+    })
+  );
+  this.transition(
+    this.fromRoute('platform.index'),
+    this.toRoute('platform.email'),
+    this.use('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toLeft', { duration } ]
+    }),
+    this.reverse('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toRight', { duration } ]
+    })
+  );
+  this.transition(
+    this.fromRoute('platform.index'),
+    this.toRoute('platform.crm'),
+    this.use('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toLeft', { duration } ]
+    }),
+    this.reverse('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toRight', { duration } ]
+    })
+  );
+  this.transition(
+    this.fromRoute('platform.index'),
+    this.toRoute('platform.profile'),
+    this.use('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toLeft', { duration } ]
+    }),
+    this.reverse('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toRight', { duration } ]
+    })
+  );
+  this.transition(
+    this.fromRoute('platform.index'),
+    this.toRoute('platform.content-curation'),
+    this.use('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toLeft', { duration } ]
+    }),
+    this.reverse('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toRight', { duration } ]
+    })
+  );
+  this.transition(
+    this.fromRoute('platform.index'),
+    this.toRoute('platform.blog'),
+    this.use('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toLeft', { duration } ]
+    }),
+    this.reverse('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toRight', { duration } ]
+    })
+  );
+  this.transition(
+    this.fromRoute('platform.index'),
+    this.toRoute('platform.calendar-events'),
+    this.use('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toLeft', { duration } ]
+    }),
+    this.reverse('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toRight', { duration } ]
+    })
+  );
+  this.transition(
+    this.fromRoute('platform.index'),
+    this.toRoute('platform.todos'),
+    this.use('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toLeft', { duration } ]
+    }),
+    this.reverse('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toRight', { duration } ]
+    })
+  );
+  this.transition(
+    this.fromRoute('platform.index'),
+    this.toRoute('platform.builder'),
+    this.use('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toLeft', { duration } ]
+    }),
+    this.reverse('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toRight', { duration } ]
+    })
+  );
+  this.transition(
+    this.fromRoute('platform.index'),
+    this.toRoute('platform.analytics'),
+    this.use('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toLeft', { duration } ]
+    }),
+    this.reverse('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toRight', { duration } ]
+    })
+  );
+  this.transition(
+    this.fromRoute('platform.index'),
+    this.toRoute('platform.messenger'),
+    this.use('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toLeft', { duration } ]
+    }),
+    this.reverse('explode', {
+      matchBy: 'data-logo-id',
+      use: ['flyTo', { duration } ]
+    }, {
+      use: ['toRight', { duration } ]
+    })
+  );
+
   this.transition(
     this.fromRoute('platform.dashboard'),
     this.toRoute('platform.estore'),
     this.use('toLeft'),
     this.reverse('toRight')
-  ),
+  );
   this.transition(
     this.fromRoute('platform.estore'),
     this.toRoute('platform.email'),
     this.use('toLeft'),
     this.reverse('toRight')
-  ),
+  );
   this.transition(
     this.fromRoute('platform.email'),
     this.toRoute('platform.crm'),
     this.use('toLeft'),
     this.reverse('toRight')
-  ),
+  );
   this.transition(
     this.fromRoute('platform.crm'),
     this.toRoute('platform.profile'),
     this.use('toRight'),
     this.reverse('toLeft')
-  ),
+  );
 
   /* Route 4 */
   this.transition(
@@ -337,43 +571,38 @@ export default function() {
     this.toRoute('platform.content-curation'),
     this.use('toLeft'),
     this.reverse('toRight')
-  ),
+  );
   this.transition(
     this.fromRoute('platform.content-curation'),
     this.toRoute('platform.blog'),
     this.use('toLeft'),
     this.reverse('toRight')
-  ),
-  this.transition(
-    this.fromRoute('platform.blog'),
-    this.toRoute('platform.calendar-events'),
-    this.use('toLeft'),
-    this.reverse('toRight')
-  ),
+  );
+
   this.transition(
     this.fromRoute('platform.calendar-events'),
     this.toRoute('platform.todos'),
     this.use('toRight'),
     this.reverse('toLeft')
-  ),
+  );
   this.transition(
     this.fromRoute('platform.todos'),
     this.toRoute('platform.builder'),
     this.use('toLeft'),
     this.reverse('toRight')
-  ),
+  );
   this.transition(
     this.fromRoute('platform.builder'),
     this.toRoute('platform.analytics'),
     this.use('toLeft'),
     this.reverse('toRight')
-  ),
+  );
   this.transition(
     this.fromRoute('platform.analytics'),
     this.toRoute('platform.messenger'),
     this.use('toLeft'),
     this.reverse('toRight')
-  ),
+  );
 
 
 
